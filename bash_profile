05674a4664822502dbbff5cbebd41b332978d9df
Thomas Bender <tbender4@gmail.com>
2019-03-11 00:24:22 -0400

    first commit

---
+++ bash_profile
@@ -0,0 +1,10 @@
+alias 125w110th="192.168.144.168"
+alias 213bay="192.168.145.168"
+alias 105chambers="192.168.146.168"
+alias 2527mercer="192.168.147.168"
+
+
+alias 125w110th_ext="50.74.59.202"
+alias 213bay_ext="108.5.233.178"
+alias 105chambers_ext="98.116.10.61"
+alias 2527mercer_ext="0.0.0.0"
---
+++ int_ext_site.py
@@ -0,0 +1,64 @@
+import csv
+import re
+
+def shortHand(matchedSite):
+    #print(matchedSite.groups())
+    #convert to shorthand
+    directionRegex = re.compile(r'North|South|East|West')
+    banRegex = re.compile(r'Road|Street|Blouevard|Lane|Park|Way|Avenue|\-| \- ')
+    output = ""
+    for address in matchedSite.groups():
+        if directionRegex.search(address) != None:
+            output += address[0].lower()  #first letter of direction, lowercase
+            continue
+        elif banRegex.search(address) != None:
+            continue
+        output += address.lower()
+    return output
+
+csvFile = open('example.csv', 'r')
+csvList = ""
+with open('example.csv') as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=',')
+    csvList = list(csvReader)
+
+def siteGeneratorFromSiteList(fullSiteList):
+    internalTemplate = "alias {}=\"192.168.{}.168\"\n"  #shortenedname, port no.
+    externalTemplate = "alias {}=\"{}\"\n"  #shortenedname, port no.
+    fullOutput = ""
+    for site in fullSiteList:
+        fullOutput += internalTemplate.format(site[1], site[0])
+    fullOutput += "\n\n"
+    for site in fullSiteList:
+        fullOutput += externalTemplate.format(site[1] + "_ext", site[2])
+    return fullOutput
+
+
+#regex_testing
+siteTypeRegex = [re.compile(r'(^\d+) (North|South|East|West)? (\d+)(th|st|nd)')]
+#125 West 110th
+#251 1st Street
+
+siteTypeRegex.append(re.compile(r'(^\d+) (\w+) (Road|Street|Blouevard|Lane|Park|Way|Avenue)'))
+#213 Bay Street(Jersey City)
+#105 Chambers Street
+
+siteTypeRegex.append(re.compile(r'(^\d+)(\-| \- )(\d+) (\w+)'))
+#25 - 27 Mercer Street
+
+#siteShortHandList = []
+fullSiteList = []
+for site in csvList:
+    matchedSite = None
+    for siteRegex in siteTypeRegex:
+        matchedSite = siteRegex.search(site[1])
+        if matchedSite != None:
+            break
+    fullSiteList.append([site[0], shortHand(matchedSite), site[2]])
+
+print("bash_profile created. Run: cat bash_profile >> ~/.bash_profile && source ./bash_profile.")
+
+
+output = siteGeneratorFromSiteList(fullSiteList)
+bashProfile = open("bash_profile", "w+")
+bashProfile.write(output)
